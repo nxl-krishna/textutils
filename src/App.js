8677b78f70b98abb4f9a50cf654d28c3daee304c
krishna rathore <krishnarathore2792005@gmail.com>
2024-12-18 13:14:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar title="TextUtils"  />
+      <div>
+        <Routes>
+
+          <Route path="/" element={<TextForm></TextForm>} />
+          <Route path='/about' element={<About></About>}></Route>
+         
+        </Routes>
+      </div>
+    </Router>
+  
+
   );
 }
 
